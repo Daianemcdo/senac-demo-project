94f10a33373733e21a035b1bde367c7f46d3ff64
Daiane Machado <daianemcdo@gmail.com>
2020-06-15 17:03:09 -0300

    Removido códigos comentados.

---
+++ demo-project/src/main/java/br/edu/sc/senac/demo/demoproject/Test.java
@@ -0,0 +1,16 @@
+package br.edu.sc.senac.demo.demoproject;
+
+public class Test {
+	private void x() {
+		System.out.println("Hello World");
+	}
+	
+	private void y() {
+		System.out.println("Alô, Terra chamando!");
+	}
+
+	private void z() {
+		System.out.println("Etê, telefone");
+	}
+
+}
